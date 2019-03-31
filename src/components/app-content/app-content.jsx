a573b0d4e4017d46d50d298d7ed4ed6a130bd18b
tienslebien <etienne@crombez.info>
2019-03-31 23:12:04 +0200

    :lipstick: update layout

---
+++ src/components/app-content/app-content.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './app-content.scss';
+
+export default function AppContent() {
+    return (
+        <>
+            <h1>Dévoilez toutes les cartes dans un temps record !</h1>
+        </>
+    );
+}
